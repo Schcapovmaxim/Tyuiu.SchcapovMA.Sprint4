e5462041afab5dfb59915745461606a5bc175a2c
Максим Щапов <maksimsapov0456@gmail.com>
2024-11-05 09:30:00 +0500

    Task4

---
+++ Tyuiu.SchcapovMA.Sprint4.Task4.V26.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+using Tyuiu.SchcapovMA.Sprint4.Task4.V26.Lib;
+namespace Tyuiu.SchcapovMA.Sprint4.Task4.V26.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            int[,] mas2 = new int[5, 5] {{5, 4, 1, 1, 2 },
+                                        { 3, 2, 4, 3, 5 },
+                                        { 5, 2, 3, 2, 2 },
+                                        { 2, 5, 1, 1, 4 },
+                                        { 5, 3, 4, 3, 2 } };
+            int[,] res = ds.Calculate(mas2);
+            int[,] wait = new int[5, 5] {{0, 4, 0, 0, 2 },
+                                        { 0, 2, 4, 0, 0 },
+                                        { 0, 2, 0, 2, 2 },
+                                        { 2, 0, 0, 0, 4 },
+                                        { 0, 0, 4, 0, 2 } };
+            CollectionAssert.AreEqual(res, wait);
+        }
+    }
+}
---
+++ Tyuiu.SchcapovMA.Sprint4.Task4.V26/Program.cs
@@ -0,0 +1,72 @@
+﻿using Tyuiu.SchcapovMA.Sprint4.Task4.V26.Lib;
+namespace Tyuiu.SchcapovMA.Sprint4.Task4.V26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнил: Щапов М.А. | ИБКСб-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Двумерные массивы.(ввод с клавиатуры)                             *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #26                                                             *");
+            Console.WriteLine("* Выполнил: Щапов Максим Александрович | ИБКСб-24-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+            Console.WriteLine("* значениями с клавиатуры в диапазоне от 1 до 5. Заменить нечетные        *");
+            Console.WriteLine("* элементы массива на 0.                                                  *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите количество строк в массиве: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите количество столбцов в массиве: ");
+            int columns = Convert.ToInt32(Console.ReadLine());
+
+            int[,] mtrx = new int[rows, columns];
+            Console.WriteLine("***************************************************************************");
+            for (int i = 0; i < rows; i++) 
+            {
+                for (int j = 0; j < columns; j++) 
+                {
+                    Console.Write($"Введите {i},{j} элемент масссива: ");
+                    mtrx[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("\nМассив:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{mtrx[i,j]} \t ");
+                    
+                }
+                Console.WriteLine();
+            }
+            mtrx = ds.Calculate(mtrx);
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("\nНовый массив:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{mtrx[i, j]} \t ");
+
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
